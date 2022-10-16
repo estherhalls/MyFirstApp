8f50a96f3b11554a67d5310d3d7b876c7ba95b84
Esther <esther@Esther-Halls-MacBook-Pro.local>
2022-10-16 17:43:01 -0600

    storyboard UI created

---
+++ MyFirstProject/MyFirstProject/Controllers/View Controllers/penTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  penTableViewCell.swift
+//  MyFirstProject
+//
+//  Created by Esther on 10/14/22.
+//
+
+import UIKit
+
+class penTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
